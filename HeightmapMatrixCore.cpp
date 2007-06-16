85004cac467c481413a7dd9961faf41489838237
tomasz.szymczyszyn <devnull@localhost>
2007-06-16 19:58:20 +0000

    Dodana mozliwosc konwersji z i na HeightmapCore. Dodana mozliwosc wykonywania wszystkich podstawowych operacji na HeightmapMatrixCore (dzieki konwersji).

--- HeightmapMatrixCore.cpp
+++ HeightmapMatrixCore.cpp
@@ -43,16 +43,56 @@ HeightmapMatrixCore::HeightmapMatrixCore(const HeightmapMatrixCore& old)
 	matrix = vector<vector<HeightmapCore> >(old.matrix);
 }
 
-HeightmapMatrixCore::HeightmapMatrixCore(const HeightmapCore& hMap, int cols, int rows)
+HeightmapMatrixCore::HeightmapMatrixCore(const HeightmapCore& hMapCore, int cols, int rows)
 {
- /* cols = 
-  mapDim = hMap.getWidth() / cols > hMap.getHeight() / rows ?
-  */
+  this->cols = cols;
+  this->rows = rows;
+  this->mapWidth = hMapCore.getWidth() / cols;
+  this->mapHeight = hMapCore.getHeight() / rows;
+  this->mapMargin = hMapCore.getMargin();
+
+  importHeightmapCore(hMapCore);
+}
+
+bool HeightmapMatrixCore::importHeightmapCore(const HeightmapCore& hMapCore)
+{
+  if (getTotalWidth() != hMapCore.getWidth() || getTotalHeight() != hMapCore.getHeight())
+    return false;
+
+  for (int i = 0; i < getTotalWidth(); ++i)
+    for (int j = 0; j < getTotalHeight(); ++j)
+      setPixel(i, j, hMapCore.getPixel(i, j));
+
+  return true;
+}
+
+HeightmapCore HeightmapMatrixCore::exportHeightmapCore() 
+{
+  HeightmapCore hMapCore(getTotalHeight(), getTotalWidth(), getMapMargin());
+
+  for (int i = 0; i < getTotalWidth(); ++i)
+    for (int j = 0; j < getTotalHeight(); ++j)
+      hMapCore.setPixel(i, j, getPixel(i, j));     
+
+  return hMapCore;
 }
 
 double HeightmapMatrixCore::getPixel(int x, int y)
 {
 	return getElement(x / mapWidth, y / mapHeight).getPixel(x % mapWidth, y % mapHeight);
+}
+
+/**
+ * @param int x wsporzedna pozioma [0, mapWidth*cols]
+ * @param int y wsporzedna pionowa [0, mapHeight*rows]
+ * @param double v  
+ */
+bool HeightmapMatrixCore::setPixel(int x, int y, double v)
+{
+  if ((x < 0) || (x > getTotalWidth() - 1) || (y < 0) || (y > getTotalHeight() - 1))
+		return false; 
+  
+  getElement(x / mapWidth, y / mapHeight).setPixel(x % mapWidth, y % mapHeight, v);  
 }
 
 double HeightmapMatrixCore::getMax()
@@ -93,6 +133,54 @@ void HeightmapMatrixCore::randomFill(double min, double max)
 	for (int i = 0; i < rows; i++)
 		for (int j = 0; j < cols; j++)
 			matrix[i][j].randomFill(min, max);
+}
+
+void HeightmapMatrixCore::clusterFill(double min, double max, short clusterChance, short radius)
+{
+  HeightmapCore hMapCore = exportHeightmapCore();
+  hMapCore.clusterFill(min, max, clusterChance, radius);
+
+  importHeightmapCore(hMapCore);
+}
+
+void HeightmapMatrixCore::clusterFilter(short radius)
+{
+  HeightmapCore hMapCore = exportHeightmapCore();
+  hMapCore.clusterFilter(radius);
+
+  importHeightmapCore(hMapCore);
+}
+
+void HeightmapMatrixCore::alternateClusterFilter(short radius)
+{
+  HeightmapCore hMapCore = exportHeightmapCore();
+  hMapCore.alternateClusterFilter(radius);
+
+  importHeightmapCore(hMapCore);
+}
+
+void HeightmapMatrixCore::liquidFilter(double c, double d, double t, double u, short iters, bool wrap)
+{
+  HeightmapCore hMapCore = exportHeightmapCore();
+  hMapCore.liquidFilter(c, d, t, u, iters, wrap);
+
+  importHeightmapCore(hMapCore);
+}
+
+void HeightmapMatrixCore::smoothFilter(short iters, short radius, bool wrap)
+{
+  HeightmapCore hMapCore = exportHeightmapCore();
+  hMapCore.smoothFilter(iters, radius, wrap);
+
+  importHeightmapCore(hMapCore);
+}
+
+void HeightmapMatrixCore::terraceFilter(short levels)
+{
+  HeightmapCore hMapCore = exportHeightmapCore();
+  hMapCore.terraceFilter(levels);
+
+  importHeightmapCore(hMapCore);
 }
 
 void HeightmapMatrixCore::wrapEdges() {
--- HeightmapMatrixCore.h
+++ HeightmapMatrixCore.h
@@ -20,32 +20,38 @@ class HeightmapMatrixCore {
   HeightmapMatrixCore(const HeightmapCore& hMap, int cols, int rows);
 	~HeightmapMatrixCore() {;}
 
+  bool importHeightmapCore(const HeightmapCore& hmapCore);
+  HeightmapCore exportHeightmapCore();
+
 	int getCols() const { return cols; }
 	int getRows() const { return rows; }
 	int getMapWidth() const { return mapWidth; }
 	int getMapHeight() const { return mapHeight; }
 	int getMapMargin() const { return mapMargin; }
+  int getTotalWidth() const { return mapWidth*cols; }
+  int getTotalHeight() const { return mapHeight*rows; }
 	HeightmapCore & getElement(int xPos, int yPos) { return matrix[yPos][xPos]; }
-	double getPixel(int x, int y); 
 	double getMax();
 	double getMin();
+
+	double getPixel(int x, int y); 
+	bool setPixel(int x, int y, double v); 
 
 
 	void flatFill(double v = 0.0);
 	void randomFill(double min = -500., double max = 500.);
 
-/*	void clusterFill(double min = -500., double max = 500.,
-	                 short clusterChance = 60, short radius = 1);
+	void clusterFill(double min = -500., double max = 500., short clusterChance = 60, short radius = 1);
 
 	void clusterFilter(short radius = 1);
+	void alternateClusterFilter(short radius = 1);
 
-	void liquidFilter(double c = 100., double d = 10., double t = .033,
-	                  double u = 100., short iters = 500, bool wrap = true);
+	void liquidFilter(double c = 100., double d = 10., double t = .033, double u = 100., short iters = 500, bool wrap = true);
 
 	void smoothFilter(short iters = 250, short radius = 1, bool wrap = true);
 
-	void wrapEdges();
-*/
+	void terraceFilter(short levels);
+
 	void wrapEdges();
 	void normalize();
 	void joinAll();
