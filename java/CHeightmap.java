b7ccd862e77d5ad3c586e2370f1dd185d10433f5
lavrin <devnull@localhost>
2010-02-12 13:30:21 +0000

    Introduced iface Heightmap implemented by CHeightmap; minor compile time bugs fixed.

---
+++ java/CHeightmap.java
@@ -0,0 +1,81 @@
+public class CHeightmap implements Heightmap
+{
+  public CHeightmap(int height, int width) {
+    this(height, width, 1);
+  }
+
+  public CHeightmap(int height, int width, int margin) {
+    _peer = create(height, width, margin);
+  }
+
+  // Heightmap interface
+  private boolean _wrap = true;
+  public void setWrapped(boolean wrap) { _wrap = wrap; }
+  public boolean isWrapped() { return _wrap; }
+
+  public native int getHeight();
+  public native int getWidth();
+  public native int getMargin();
+  public native double getPixel(int x, int y);
+  public native double getMax();
+  public native double getMin();
+
+  public native boolean setPixel(int x, int y, double value);
+
+  // fills
+  public native void flatFill(double v);
+  public native void randomFill(double min, double max);
+  public native void clusterFill(double min, double max,
+    short clusterChance, short radius);
+
+  // filters
+  public native void clusterFilter(int radius);
+  public native void alternateClusterFilter(short radius);
+
+  public void liquidFilter(double c, double d, double t, double u) {
+    liquidFilter(c, d, t, u, _wrap);
+  }
+  private native void liquidFilter(double c, double d, double t, double u, boolean wrap);
+
+  public void smoothFilter(short radius) {
+    smoothFilter(radius, _wrap);
+  }
+  private native void smoothFilter(short radius, boolean wrap);
+
+  public native void terraceFilter(short levels);
+
+  public void walkerFilter(int incStep, int decStep) {
+    walkerFilter(incStep, decStep, _wrap);
+  }
+  private native void walkerFilter(int incStep, int decStep, boolean wrap);
+
+  public void faultingFilter(int incStep, int decStep) {
+    faultingFilter(incStep, decStep);
+  }
+  private native void faultingFilter(int incStep, int decStep, boolean wrap);
+
+  public native void normalize();
+  public native void normalize(double max, double min);
+
+  public native boolean hasMask();
+
+  // native interface stuff
+  private long _peer;  // pointer to native counterpart
+
+  private native long create(int height, int width, int margin);
+  private native void destroy(long peer);
+  public void destroy()
+  {
+    destroy(_peer);
+  }
+
+  protected void finalize()
+  {
+    destroy();
+  }
+
+  static
+  {
+    System.loadLibrary("hmap");
+  }
+}
